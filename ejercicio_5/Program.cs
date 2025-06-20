bbced993e6d5be499cd33b8673ce72736240a33f
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 16:49:06 -0500

    terminado

--- ejercicio_5/Program.cs
+++ ejercicio_5/Program.cs
@@ -29,7 +29,6 @@ namespace ejercicio_5
             Console.WriteLine("La resta es: "+resta);
             //esta es la forma en la que puedes poner varias respuestas en una linea
             Console.WriteLine("La multiplicación es: {0}, la suma es: {1}", multi, suma);
-            //hola erick
         }
     }
 }
